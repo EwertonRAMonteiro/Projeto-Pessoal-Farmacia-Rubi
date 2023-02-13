08fcb1fc2b8cb1c11fe888923cdd9c24de29eacc
EwertonRAMonteiro <ewertonra.monteiro@gmail.com>
2023-02-13 19:06:25 -0300

    first commit

---
+++ bacEndJava/src/main/java/br/com/farmacia/rubi/FarmaciaRubiApplication.java
@@ -0,0 +1,13 @@
+package br.com.farmacia.rubi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaciaRubiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaciaRubiApplication.class, args);
+	}
+
+}
---
+++ bacEndJava/src/test/java/br/com/farmacia/rubi/FarmaciaRubiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.farmacia.rubi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaciaRubiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
