d4e4f1e416604e72b6b8e61843e57d4416631958
EwertonRAMonteiro <ewertonra.monteiro@gmail.com>
2023-02-17 21:21:38 -0300

    referenciando entidade correnta

--- bacEndJava/src/main/java/br/com/farmacia/rubi/repositories/EnderecoForRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.farmacia.rubi.repositories;
-
-import br.com.farmacia.rubi.entities.EnderecoFor;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EnderecoForRepository extends JpaRepository<EnderecoFor,Long> {
-}
