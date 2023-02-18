7229b1f2afc27499a35ee4148099515925c7871a
EwertonRAMonteiro <ewertonra.monteiro@gmail.com>
2023-02-17 21:19:27 -0300

    dto de request funcionario

---
+++ bacEndJava/src/main/java/br/com/farmacia/rubi/dto/FuncionarioRequest.java
@@ -0,0 +1,40 @@
+package br.com.farmacia.rubi.dto;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class FuncionarioRequest {
+
+    private String firstName;
+
+    private String midName;
+
+    private String lastName;
+
+    private Date dataNasc;
+
+    private String email;
+
+    private Integer telefone;
+
+    private Float salario;
+
+    private String funcao;
+
+    private Byte credencial;
+
+    private Integer cpf;
+
+    private Date dataTime;
+
+}
