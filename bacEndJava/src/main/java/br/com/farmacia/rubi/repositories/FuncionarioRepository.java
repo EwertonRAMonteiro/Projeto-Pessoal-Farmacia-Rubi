d39a3a220056a539023851750b3e50c1be61a198
EwertonRAMonteiro <ewertonra.monteiro@gmail.com>
2023-02-20 22:17:00 -0300

    tratamento de exception

--- bacEndJava/src/main/java/br/com/farmacia/rubi/repositories/FuncionarioRepository.java
+++ bacEndJava/src/main/java/br/com/farmacia/rubi/repositories/FuncionarioRepository.java
@@ -3,6 +3,8 @@ package br.com.farmacia.rubi.repositories;
 import br.com.farmacia.rubi.entities.Funcionario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
 
 
 @Repository
