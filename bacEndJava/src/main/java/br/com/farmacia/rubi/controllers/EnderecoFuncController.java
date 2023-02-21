8b01117ed0d7bb0129df44109c84cf85141fa1b9
EwertonRAMonteiro <ewertonra.monteiro@gmail.com>
2023-02-20 22:14:39 -0300

    controladores

---
+++ bacEndJava/src/main/java/br/com/farmacia/rubi/controllers/EnderecoFuncController.java
@@ -0,0 +1,12 @@
+package br.com.farmacia.rubi.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@CrossOrigin(origins = "*")
+@RequestMapping("farmaciarubi")
+public class EnderecoFuncController {
+
+}
---
+++ bacEndJava/src/main/java/br/com/farmacia/rubi/controllers/FuncionarioController.java
@@ -0,0 +1,65 @@
+package br.com.farmacia.rubi.controllers;
+
+import br.com.farmacia.rubi.ResourceNotFoundException;
+import br.com.farmacia.rubi.dto.FuncionarioRequest;
+import br.com.farmacia.rubi.entities.Funcionario;
+import br.com.farmacia.rubi.repositories.FuncionarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+@CrossOrigin(origins = "*")
+@RequestMapping("/farmaciarubi")
+@RestController
+public class FuncionarioController {
+
+    @Autowired
+    private FuncionarioRepository funcionarioRepository;
+
+    @GetMapping("/funcionarios")
+    public List<Funcionario> getAllFuncionarios(){
+        return funcionarioRepository.findAll();
+    }
+
+    @GetMapping("funcionarios/{id}")
+    public ResponseEntity<Funcionario> getFuncionarioById(@PathVariable Long id) {
+        Funcionario funcionario = funcionarioRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("funcionario inexistente"));
+        return ResponseEntity.ok(funcionario);
+    }
+
+    @PostMapping("funcionarios")
+    public Funcionario createFuncionario(@RequestBody FuncionarioRequest request) {
+        return funcionarioRepository.save(Funcionario.of(request));
+    }
+
+    @PutMapping("funcionarios/{id}")
+    public ResponseEntity<Funcionario> updateFuncionario(@PathVariable Long id, FuncionarioRequest request){
+        funcionarioRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("funcionario não existe"));
+        var funcionario = Funcionario.of(request);
+        funcionario.setId(id);
+
+        Funcionario newFuncionario = funcionarioRepository.save(funcionario);
+
+        return ResponseEntity.ok(newFuncionario);
+    }
+
+    @DeleteMapping("funcionarios/{id}")
+    public ResponseEntity<Map<String, Boolean>> deleteFuncionario(@PathVariable Long id) {
+        Funcionario funcionario = funcionarioRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("funcionario não deletado"));
+
+        funcionarioRepository.delete(funcionario);
+        Map<String, Boolean> response = new HashMap<>();
+        return ResponseEntity.ok(response);
+    }
+
+
+}
