07cca60185252e4acc9379097de8d675229e7808
EwertonRAMonteiro <ewertonra.monteiro@gmail.com>
2023-05-04 19:52:58 -0300

    criando componentes para desenvolvimento, login pronto

--- frontEndAngular/farmacia-rubi/src/app/app.module.ts
+++ frontEndAngular/farmacia-rubi/src/app/app.module.ts
@@ -4,14 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './componetes/header/header.component';
-import { FooterComponent } from './componetes/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { FuncionariosComponent } from './pages/funcionarios/funcionarios.component';
+import { FornecedoresComponent } from './pages/fornecedores/fornecedores.component';
+import { EstoqueComponent } from './pages/estoque/estoque.component';
+import { VendasComponent } from './pages/vendas/vendas.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent,
+    HomeComponent,
+    LoginComponent,
+    FuncionariosComponent,
+    FornecedoresComponent,
+    EstoqueComponent,
+    VendasComponent,
+
 
   ],
   imports: [
